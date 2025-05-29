5745c9c344d13ca1bb982da336828187eb55d33e
MarceloLuan <marcelobluan@gmail.com>
2025-05-29 20:52:44 -0300

    adicionando funções básicas ao nó

--- src/Arvore.java
+++ src/Arvore.java
@@ -12,6 +12,7 @@ public class Arvore {
         this.raiz = null;
     }
 
+    
     //método para contar os nós total da árvore
     public int contarNos(No no) {
         if (no == null) {
--- src/No.java
+++ src/No.java
@@ -7,4 +7,38 @@ public class No {
         this.esquerda = null;
         this.direita = null;
     }
+
+    //função adicionar nó à esquerda
+    public void adicionarEsquerda(No no) {
+        this.esquerda = no;
+    }
+    //função adicionar nó à direita
+    public void adicionarDireita(No no) {
+        this.direita = no;
+    }
+    //função para retornar o valor do nó
+    public String getValor() {
+        return valor;
+    }
+    //função para retornar o nó da esquerda
+    public No getEsquerda() {
+        return esquerda;
+    }
+    //função para retornar o nó da direita
+    public No getDireita() {
+        return direita;
+    }
+    //função para definir o valor do nó
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    //função para definir o nó da esquerda
+    public void setEsquerda(No esquerda) {
+        this.esquerda = esquerda;
+    }
+    //função para definir o nó da direita
+    public void setDireita(No direita) {
+        this.direita = direita;
+    }
+
 }
