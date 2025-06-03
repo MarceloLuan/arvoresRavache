57825c9788f17ef06fb8649b449c009e65666040
MarceloLuan <marcelobluan@gmail.com>
2025-06-03 20:00:49 -0300

    rotacao direita noAVL

---
+++ src/ArvoreAVL.java
@@ -0,0 +1,44 @@
+public class ArvoreAVL {
+    public NoAVL raiz;
+
+    public int altura(NoAVL no) {
+        if (no == null) {
+            return 0;
+        }
+        return no.altura;
+    }
+
+    //calcula o fator de balanceamento do nó
+    public int fatorBalanceamento(NoAVL no) {
+        if (no == null) {
+            return 0;
+        }
+        return altura(no.esquerda) - altura(no.direita);
+    }
+
+    //atualiza a altura do nó
+    public void atualizarAltura(NoAVL no) {
+        if (no != null) {
+            no.altura = 1 + Math.max(altura(no.esquerda), altura(no.direita));
+            //pesquisando sobre o math.max, ele vai retornar o maior valor entre os dois valores passados
+            //e o 1 é para considerar o nó atual na altura
+        }
+    }
+
+    //criando a rotação à direita
+    public rotacaoDireita(NoAVL y){
+        NoAVL x = y.esquerda;
+        NoAVL subArvoreDireitadeX = x.direita
+
+        x.direita = y;
+        y.esquerda = subArvoreDireitadeX;
+
+        atualizarAltura(y);
+        atualizarAltura(x);
+
+        return x; //retorna o novo nó raiz
+
+
+    }
+
+}
