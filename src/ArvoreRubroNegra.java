e0992bfede2ff7e18a6773f10397c6b89025692a
MarceloLuan <marcelobluan@gmail.com>
2025-06-12 21:47:25 -0300

    adicionando rotações

--- src/ArvoreRubroNegra.java
+++ src/ArvoreRubroNegra.java
@@ -5,4 +5,50 @@ public class ArvoreRubroNegra {
         this.raiz = null;
     }
 
+    //criando rotação à direita para rubro negra
+    public NoRubroNegra rotacaoEsquerda(NoRubroNegra no){
+        NoRubroNegra y = no.direita;
+        no.direita = y.esquerda;
+        if (y.esquerda != null) {
+            y.esquerda.pai = no;
+        }
+
+        y.pai = no.pai;
+
+        if (no.pai == null) {
+            this.raiz = y;//se o nó for raiz, atualiza a raiz
+        } else if (no == no.pai.esquerda) {
+            no.pai.esquerda = y;//se o nó for filho esquerdo, atualiza o filho esquerdo do pai
+        } else {
+            no.pai.direita = y;// se o nó for filho direito, atualiza o filho direito do pai
+        }
+        y.esquerda = no;
+        no.pai = y;
+
+        return y;
+    }
+
+    //método rotação direita rubro negra
+    public NoRubroNegra rotacaoDireita(NoRubroNegra no){
+        NoRubroNegra y = no.esquerda;
+        no.esquerda = y.direita;
+        if (y.direita != null) {
+            y.direita.pai = no;
+        }
+
+        y.pai = no.pai;
+
+        if (no.pai == null) {
+            this.raiz = y;//se o nó for raiz, atualiza a raiz
+        } else if (no == no.pai.direita) {
+            no.pai.direita = y;//se o nó for filho direito, atualiza o filho direito do pai
+        } else {
+            no.pai.esquerda = y;//se o nó for filho esquerdo, atualiza o filho esquerdo do pai
+        }
+        y.direita = no;
+        no.pai = y;
+
+        return y;
+    }
+
 }
